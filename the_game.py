7a4ce8b139143a3baa0bc4c514dcb7706d21c93b
CarolLu85 <little_shrewdness@hotmail.com>
2023-09-09 22:41:32 +0300

    add difficulty function and clear screen function

--- the_game.py
+++ the_game.py
@@ -1,10 +1,18 @@
 from art import logo
 import random
-print("Welcome to the YiYi's game")
-print(logo)
-ACTUAL_NUMBER = random.randint(1,100)
-print("I'm thinking of a number between 1 and 100")
-end_of_game = False
+import os
+# print("Welcome to the YiYi's game")
+# print(logo)
+# ACTUAL_NUMBER = random.randint(1,100)
+# print("I'm thinking of a number between 1 and 100")
+# end_of_game = False
+
+def difficulty():
+  game_level = input("please choose a game level, type 'easy' or 'hard'\n")
+  if game_level == "easy":
+    return 10
+  elif game_level == "hard":
+    return 5  
 
 def play_game(turns):
   while turns > 0:
@@ -18,18 +26,19 @@ def play_game(turns):
       print(f"you got the right number, the number is {ACTUAL_NUMBER}")
     turns -= 1
     print(f"You have {turns} times left")
-    print(f"The number is {ACTUAL_NUMBER}")
+  print(f"The number is {ACTUAL_NUMBER}")
 
-
-
+end_of_game = False
 while not end_of_game:
-  game_level = input("please choose a game level, type 'easy' or 'hard'\n")
-  if game_level == "easy":
-    attempt = 10
-  elif game_level == "hard":
-    attempt = 5
+  print("Welcome to the YiYi's game")
+  print(logo)
+  ACTUAL_NUMBER = random.randint(1,100)
+  print("I'm thinking of a number between 1 and 100")
+  attempt = difficulty()
   play_game(attempt)
   ask_again = input("do you want to continue? type 'Y' or 'N'").lower()
   if ask_again == "n":
     print("The End")
     end_of_game = True
+  else:
+    os.system('cls')
